b2849eae89c9c74311a8ece2772ba791b42e4350
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-05-24 15:32:48 +0100

    Add functioanlity to select appointmet slot and date validation

--- appointments/models.py
+++ appointments/models.py
@@ -1,13 +1,34 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
 
 class Appointment(models.Model):
+    class Slot(models.IntegerChoices):
+        NINE = 900, '9:00'
+        HALFNINE = 950, '9:30'
+        TEN = 1000, '10:00'
+        HALFTEN = 1050, '10:30'
+        ELEVEN = 1100, '11:00'
+        HALFELEVEN = 1150, '11:30'
+        TWELVE = 1200, '12:00'
+        HALFTWELVE = 1250, '12:30'
+        THIRTEEN = 1300, '13:00'
+        HALFTHIRTEEN = 1350, '13:30'
+        FOURTEEN = 1400, '14:00'
+        HALFFOURTEEN = 1450, '14:30'
+        FIFTEEN = 1500, '15:00'
+        HALFFIFTEEN = 1550, '15:30'
+        SIXTEEN = 1600, '16:00'
+        HALFSIXTEEN = 1650, '16:30'
+
+    
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     client_name = models.CharField(max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    date = models.DateTimeField(unique=True)
+    date = models.DateField()
+    time = models.IntegerField(choices=Slot.choices, default=Slot.NINE)
     notes = models.TextField(blank=True, null=True)
 
     class Meta:
--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 from .models import Appointment
+from django.utils import timezone
+
+import math
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -9,11 +12,16 @@ class AppointmentSerializer(serializers.ModelSerializer):
     the logged in user automatically """
     owner = serializers.ReadOnlyField(source='owner.username')
 
+    def validate(self, data):
+        # Clients can book appointments for the following day or after.
+        if data['date'] <= timezone.now().date():
+            raise serializers.ValidationError("This date is not available.")
+
     class Meta:
         model = Appointment
         fields = [
             'id', 'owner', 'created_at', 'updated_at',
-            'date', 'notes',
+            'date', 'time', 'notes',
         ]
 
 
@@ -23,16 +31,38 @@ class ClientAppointmentSerializer(serializers.ModelSerializer):
     Staff members can select an existing user as owner """
 
     def validate(self, data):
-        """
-        Check if owner is null, in that case we make name mandatory.
-        """
+        # Check if owner is null, in that case we make name mandatory.
         if data['owner'] is None and data['client_name'] == "":
             raise serializers.ValidationError("Please, enter a name for unregistered users")
+        
+        # Check that appointment date is not in the past.
+        if data['date'] < timezone.now().date():
+            raise serializers.ValidationError("Appointment cannot be in the past")
+        
+        # If appointment is for today, check that time slot is not in the past
+        if data['date'] == timezone.now().date():
+            # This is the current hour rounded down
+            current_time = (timezone.now().time().hour) * 100
+            # Users cannot book for current hour, so we add 100
+            first_available = current_time + 100
+            # If the current time is in the second half of the hour,
+            # we postpone the available spot of another half an hour
+            if timezone.now().time().minute > 29:
+                first_available += 50
+            selected_slot = data['time']
+            # Users cannot book any spot before the first available
+            if selected_slot < first_available:
+                hour = math.floor(first_available / 100)
+                minutes = "00" if (first_available % 100) < 50 else "30"
+                raise serializers.ValidationError(f"The first available spot for today is at {hour}:{minutes}")
+        
         return data
+    
+        
 
     class Meta:
         model = Appointment
         fields = [
             'id', 'owner', 'client_name', 'created_at', 'updated_at',
-            'date', 'notes',
+            'date', 'time', 'notes',
         ]
