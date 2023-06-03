c397ac04a045317609c752fa5c74b5f8a315bc36
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-03 18:38:39 +0100

    Move allowed host to env

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -68,12 +68,15 @@ REST_AUTH_SERIALIZERS = {
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if 'DEV' in os.environ else False
 
-ALLOWED_HOSTS = ['localhost', 'platinum-booking-api.herokuapp.com']
+ALLOWED_HOSTS = [
+     os.environ.get('ALLOWED_HOST'),
+     'localhost',
+     ]
 
 
 # Application definition
