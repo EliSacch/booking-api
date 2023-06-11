ea59fd1dc5e3a8ea8a74f2363545a29114913992
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-11 11:01:44 +0100

    Add Client Origin to settings.py

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import re
 import dj_database_url
 
 
@@ -75,10 +76,24 @@ DEBUG = True if 'DEV' in os.environ else False
 
 ALLOWED_HOSTS = [
      os.environ.get('ALLOWED_HOST'),
-     'platinum-booking-api.herokuapp.com',
      'localhost',
      '127.0.0.1',
      ]
+
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
