9645aabee0f10a06a2578697043e959ed8aec725
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-12 14:38:50 +0100

    Add gitpod to client origin dev

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -129,12 +129,12 @@ MIDDLEWARE = [
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
-        "http://localhost:3000",
     ]
-"""if 'CLIENT_ORIGIN_DEV' in os.environ:
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
-        os.environ.get('CLIENT_ORIGIN_DEV'),
-    ]"""
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
