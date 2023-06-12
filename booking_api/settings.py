959d47b7b78af4d674b5e88eeff143b5607e3eca
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-12 15:45:22 +0100

    Remove gitpod from ALLOWED_ORIGIN

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -130,11 +130,15 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
     ]
-if 'CLIENT_ORIGIN_DEV' in os.environ:
+"""if 'CLIENT_ORIGIN_DEV' in os.environ:
     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
     CORS_ALLOWED_ORIGIN_REGEXES = [
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
         "http://localhost:3000/",
+    ]"""
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        os.environ.get('CLIENT_ORIGIN_DEV'),
     ]
 
 CORS_ALLOW_CREDENTIALS = True
