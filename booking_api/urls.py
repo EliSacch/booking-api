40b2de006e5b8dd6a184f14d32c84a4c3ae46575
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-13 16:03:18 +0100

    Add custom logout route

--- booking_api/urls.py
+++ booking_api/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import root_route
+from .views import root_route, logout_route
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +8,8 @@ urlpatterns = [
     path('', root_route),
     # To allow users to log in and out of API in-browser interface
     path('api_auth/', include('rest_framework.urls')),
+    # custom logout route
+    path('dj-rest-auth/logout/', logout_route),
     # To use dj-rest-auth
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path(
--- booking_api/views.py
+++ booking_api/views.py
@@ -11,3 +11,28 @@ def root_route(request):
     return Response({
         "message": "Welcome to Booking API!"
     })
+
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
