cd3b4a5d8e5b1ee04105476d523a9a71a362210f
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-06-13 16:21:28 +0100

    add WT_AUTH_HTTPONLY to settings

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -56,9 +56,10 @@ if 'DEV' not in os.environ:
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
-JWT_AUTH_COOKIE = 'booking-api-auth'
+JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
+JWT_AUTH_HTTPONLY = False
 
 # Override the default user detail serializer
 REST_AUTH_SERIALIZERS = {
