3baf9785f358dc10c706ea55b26c1f7ea68c19ab
elisacch <elisa.sacchelli@icloud.com>
2023-09-27 12:58:48 +0100

    Add cors whitelist

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -86,6 +86,11 @@ ALLOWED_HOSTS = [
      '127.0.0.1',
      ]
 
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    os.environ.get('ALLOWED_CORS'),
+]
+
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
